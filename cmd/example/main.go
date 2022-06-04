7ab8b3a58d2d714d9903a99ad4f82ce3aa54a2d9
andreykurzantsev <hawkeye54123@gmail.com>
2022-06-04 15:57:03 +0300

    modify main file; write working with input handler

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,24 +4,30 @@ import (
 	"flag"
 	"fmt"
 	lab2 "github.com/Makov-Vik/postfix-to-infix.git"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputExpression = flag.String("e", "17 10 + 3 * 9 /", "Expression to compute")
+	fileInput       = flag.String("f", "", "File with expression to compute")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	var Input io.Reader
 
-	res, _ := lab2.PostfixToInfix("+ 2 2")
-	fmt.Println(res)
+	if len(*fileInput) > 0 {
+		file, err := os.Open(*fileInput)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: Such file does not exist.")
+			return
+		}
+		Input = file
+	} else {
+		Input = strings.NewReader(*inputExpression)
+	}
+	//TODO work with output and with handler
 }
