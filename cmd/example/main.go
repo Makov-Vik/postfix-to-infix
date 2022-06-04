c9f5d4bfd3e2f419ab2c0daaff05ec088f2d4c5d
Makiv-Vik <viktorkaktysikov@gmail.com>
2022-06-04 16:03:25 +0300

    modify main function and working with output handler

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -12,12 +12,14 @@ import (
 var (
 	inputExpression = flag.String("e", "17 10 + 3 * 9 /", "Expression to compute")
 	fileInput       = flag.String("f", "", "File with expression to compute")
+	fileOutput      = flag.String("o", "", "File to expression output")
 )
 
 func main() {
 	flag.Parse()
 
 	var Input io.Reader
+	var Output io.Writer
 
 	if len(*fileInput) > 0 {
 		file, err := os.Open(*fileInput)
@@ -29,5 +31,25 @@ func main() {
 	} else {
 		Input = strings.NewReader(*inputExpression)
 	}
-	//TODO work with output and with handler
+
+	if len(*fileOutput) > 0 {
+    file, err := os.OpenFile(*fileOutput, os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "Error: Such file does not exist.")
+      return
+    }
+    Output = file
+  } else {
+    Output = os.Stdout
+  }
+
+  handler := &lab2.ComputeHandler{
+    Input:  Input,
+    Output: Output,
+  }
+  err := handler.Compute()
+  if err != nil {
+    fmt.Fprintf(os.Stderr, err.Error())
+    return
+  }
 }
