72e0ed3bd4e48c57b48e753fae462b6de9a98b43
andreykurzantsev <hawkeye54123@gmail.com>
2022-06-04 15:10:11 +0300

    create implementation.go

--- implementation.go
+++ implementation.go
@@ -1,9 +1,45 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixToInfix(input string) (string, error) {
+	inputArr := strings.Split(input, " ")
+
+	var signCount int
+	var numberCount int
+	buffer := make([]string, 0)
+
+	for _, operator := range inputArr {
+		if operator == "^" || operator == "*" || operator == "/" || operator == "+" || operator == "-" {
+			signCount++
+		} else {
+			numberCount++
+		}
+	}
+
+	if signCount != numberCount-1 {
+		return "", errors.New("Error: Invalid postfix equation")
+	}
+
+	for i := 0; i <= len(inputArr)-1; i++ {
+
+		if inputArr[i] != "^" && inputArr[i] != "*" && inputArr[i] != "/" && inputArr[i] != "+" && inputArr[i] != "-" {
+			buffer = append(buffer, inputArr[i])
+		} else {
+			if len(buffer) >= 2 {
+				operand1 := buffer[len(buffer)-1]
+				buffer = buffer[:len(buffer)-1]
+				operand2 := buffer[len(buffer)-1]
+				buffer = buffer[:len(buffer)-1]
+				buffer = append(buffer, "("+operand2+inputArr[i]+operand1+")")
+			} else {
+				return "", errors.New("Error: Invalid postfix equation")
+			}
+		}
+	}
+
+	return buffer[0][1 : len(buffer[0])-1], nil
 }
