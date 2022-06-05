22b22c18e7c9e54497c3b24e9be2b2ab4fa72b1c
Ervin <70893070+Er4ik@users.noreply.github.com>
2022-06-05 21:48:20 +0300

    added tests for implementation

--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,62 @@
 package lab2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPostfixToInfix(t *testing.T) {
-	res, err := PostfixToInfix("17 10 + 3 * 9 /")
+func SuccessfullyTestPostfixToInfix1(t *testing.T) {
+	res, err := PostfixToInfix("234 2 * 3 + 2 /")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "((17+10)*3)/9", res)
+		assert.Equal(t, "((234*2)+3)/2", res)
 	}
 }
 
-func ExamplePostfixToInfix() {
-	res, _ := PostfixToInfix("8 5 *")
-	fmt.Println(res)
+func SuccessfullyTestPostfixToInfix2(t *testing.T) {
+	res, err := PostfixToInfix("15 2 2.5 * / 32 + 2 /")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "((15/(2*2.5))+32)/2", res)
+	}
+}
 
-	// Output:
-	// 8 5 *
+func SuccessfullyTestPostfixToInfix3(t *testing.T) {
+	res, err := PostfixToInfix("32 1 + 15 3 / 2 * 1 / 22 * -")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(32+1)-((((15/3)*2)/1)*22)", res)
+	}
+}
+
+func SuccessfullyTestPostfixToInfix4(t *testing.T) {
+	res, err := PostfixToInfix("0.75 225 * 1 - 2 / 32 - 38 2 * +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "((((0.75*225)-1)/2)-32)+(38*2)", res)
+	}
+}
+
+func SuccessfullyTestPostfixToInfix5(t *testing.T) {
+	res, err := PostfixToInfix("1283 2 * 777 111 / * 228 114 / 113 1 + 2 / / +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "((1283*2)*(777/111))+((228/114)/((113+1)/2))", res)
+	}
+}
+
+func ErrorTestPostfixToInfix1(t *testing.T) {
+	_, err := PostfixToInfix("* 15 + 23 17 / + 53 62 + +")
+	assert.NotNil(t, err)
+}
+
+func ErrorTestPostfixToInfix2(t *testing.T) {
+	_, err := PostfixToInfix("1 2 3 4 5")
+	assert.NotNil(t, err)
+}
+
+func ErrorTestPostfixToInfix3(t *testing.T) {
+	_, err := PostfixToInfix(" ")
+	assert.NotNil(t, err)
+}
+
+func ErrorTestPostfixToInfix4(t *testing.T) {
+	_, err := PostfixToInfix("")
+	assert.NotNil(t, err)
 }
